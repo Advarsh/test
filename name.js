fc5b9cc22d839865b7f5b807ec0a060b92d2b20c
unknown <adarshvaidya00@gmail.com>
2022-06-08 10:05:54 +0530

    first commit

---
+++ name.js
@@ -0,0 +1,8 @@
+//local
+const secret = 'secret message';
+// global
+const john = 'john';
+const sarah = 'sarah';
+
+//inorder to export the content of this module we use module.exports
+module.exports = {john, sarah};
